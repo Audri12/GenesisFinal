fe936b79fbb16c4733a458ee3a1621ba2b5b1d2a
Mashrufa Mehnaz <mashrufamehnaz@gmail.com>
2024-09-29 22:41:17 +0600

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 
-import TestimonialSlider from "./Component/About/AbouOurTestimonials"
-import AboutBannerLayout from "./Component/About/AboutBannerLayout"
+
 import AboutBannerwithbg from "./Component/About/AboutBannerwithbg"
 import AboutTopBanner from "./Component/About/AboutTopBanner"
 import MeetTheTeam from "./Component/About/MeetTheTeam"
 import TestimonialWithBg from "./Component/About/TestimonialWithBg"
-import ContactUs from "./Component/Pages/Contact Us/ContactUs"
+
 import ContactUsLayout from "./Component/Pages/Contact Us/ContactUsLayout"
 import NewsBanner from "./Component/Pages/News/NewsBanner"
 import NewsClassicLayout from "./Component/Pages/News/NewsClassicLayout"
 import AllMembers from "./Component/Pages/OurTeam/AllMembers"
 import OurTeamBanner from "./Component/Pages/OurTeam/OurTeamBanner"
 import TeamFAQWithBG from "./Component/Pages/OurTeam/TeamFAQWithBG"
-import TeamSectionFAQ from "./Component/Pages/OurTeam/TeamSectionFAQ"
+
 
 
 function App() {
@@ -26,7 +25,7 @@ function App() {
     <MeetTheTeam></MeetTheTeam>
     <OurTeamBanner></OurTeamBanner>
     <AllMembers></AllMembers>
-    <TeamSectionFAQ></TeamSectionFAQ>
+    
     <TeamFAQWithBG></TeamFAQWithBG>
     <NewsBanner></NewsBanner>
     <NewsClassicLayout></NewsClassicLayout>
