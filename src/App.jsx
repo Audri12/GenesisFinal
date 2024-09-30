0d187e131f3c25757e8191481d53bffe0e409aef
Mashrufa Mehnaz <mashrufamehnaz@gmail.com>
2024-09-30 12:49:05 +0600

    banner-Carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 
 
+import Carousel from "react-multi-carousel"
 import AboutBannerwithbg from "./Component/About/AboutBannerwithbg"
 import AboutTopBanner from "./Component/About/AboutTopBanner"
 import MeetTheTeam from "./Component/About/MeetTheTeam"
 import TestimonialWithBg from "./Component/About/TestimonialWithBg"
 
 import ContactUsLayout from "./Component/Pages/Contact Us/ContactUsLayout"
+import HomeLayout1 from "./Component/Pages/HomeLayout/HomeLayout1"
 import NewsBanner from "./Component/Pages/News/NewsBanner"
 import NewsClassicLayout from "./Component/Pages/News/NewsClassicLayout"
 import AllMembers from "./Component/Pages/OurTeam/AllMembers"
 import OurTeamBanner from "./Component/Pages/OurTeam/OurTeamBanner"
 import TeamFAQWithBG from "./Component/Pages/OurTeam/TeamFAQWithBG"
+import Carousel1 from "./Component/Pages/HomeLayout/Carousel1"
 
 
 
@@ -30,6 +33,10 @@ function App() {
     <NewsBanner></NewsBanner>
     <NewsClassicLayout></NewsClassicLayout>
     <ContactUsLayout></ContactUsLayout>
+    {/* <HomeLayout1></HomeLayout1> */}
+    <Carousel1></Carousel1>
+  
+    
     
      
     </>
